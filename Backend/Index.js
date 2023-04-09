edbeb3b5e6cc065e9d11e1f1b107804fd3108b22
Zander99900 <dipankarkarmakardk007@gmail.com>
2023-04-09 13:48:37 +0530

    Added Express js connection framework

--- Backend/Index.js
+++ Backend/Index.js
@@ -1,3 +1,15 @@
-const connectToMongo = require('./db');
+const connectToMongo = require("./db");
+const express = require("express");
 
 connectToMongo();
+//Connected to Express Js
+const app = express();
+const port = 3000;
+
+app.get("/", (req, res) => {
+  res.send("Hello Zander!");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
--- Backend/db.js
+++ Backend/db.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
-var mongoURI = "mongodb://localhost:27017/?readPreference=primary"
-connectToMongo().catch(err => console.log(err));
+var mongoURI = "mongodb://0.0.0.0:27017/?readPreference=primary"
+// connectToMongo().catch(err => console.log(err));
 
 async function connectToMongo() {
   await mongoose.connect(mongoURI);
